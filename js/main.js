36b827bc204525c82565ce38835be1a47f5854a6
BenjamÃin Zamboni <benjaminjzamboni@gmail.com>
2023-01-28 11:04:04 -0300

    Selección y costo neto

--- js/main.js
+++ js/main.js
@@ -1,3 +1,48 @@
- //comentario1
+//PreEntrega1
 
-/*Primera clase*/
+let nombre = prompt("Ingrese su nombre");
+
+console.log("El nombre ingresado es: " + nombre);
+
+let apellido = prompt("Ingrese su apellido");
+
+console.log("El nombre ingresado es: " + apellido);
+
+let edad = parseInt(prompt("Ingrese su edad"));
+
+console.log("El nombre ingresado es: " + edad);
+
+let modeloElegido = prompt(
+  "Ingrese el par de sandalias que desea agregar al carrito (Ingrese SALIR si no quiere evaluar el costo"
+);
+
+let costoDelPar = 0;
+let costoAcumulado = 0;
+
+while (modeloElegido !== "SALIR") {
+  switch (modeloElegido) {
+    case "TARAHUMARA":
+    case "PIRIPKURÁ":
+      costoDelPar = 3200;
+      break;
+
+    case "RARAMURÍ":
+    case "KAWAHIVA":
+      costoDelPar = 4500;
+      break;
+  }
+
+  console.log("El costo es : $" + costoDelPar);
+  let aniadir = prompt("Añadir al carrito (Si/No) ");
+
+  If(aniadir == "Si");
+  {
+    costoAcumulado += costoDelPar;
+    console.log("El costo Acumulado es : $" + costoAcumulado);
+  }
+  modeloElegido = prompt(
+    "Ingrese un nuevo par de sandalias(Ingrese SALIR si no quiere evaluar el costo"
+  );
+}
+
+
